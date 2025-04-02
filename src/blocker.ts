e442ae49b452c711a742a4c2908f76356bc67293
Tim Cheung <tim@cheung.io>
2025-04-02 20:50:13 +0100

    add blocker

---
+++ src/blocker.ts
@@ -0,0 +1,81 @@
+import fs from "node:fs/promises";
+import {
+	FiltersEngine,
+	Request,
+	adsAndTrackingLists,
+	adsLists,
+} from "@ghostery/adblocker";
+import type { Session } from "electron";
+import { config } from "./lib/config";
+
+export class Blocker {
+	private _engine: FiltersEngine | undefined;
+
+	get engine() {
+		if (!this._engine) {
+			throw new Error("Engine not initialized");
+		}
+
+		return this._engine;
+	}
+
+	set engine(engine: FiltersEngine) {
+		this._engine = engine;
+	}
+
+	async init() {
+		if (!config.get("blocker.enabled")) {
+			return;
+		}
+
+		const lists =
+			config.get("blocker.ads") && config.get("blocker.tracking")
+				? adsAndTrackingLists
+				: config.get("blocker.ads")
+					? adsLists
+					: config.get("blocker.tracking")
+						? adsAndTrackingLists.filter((list) => !adsLists.includes(list))
+						: [];
+
+		if (!lists.length) {
+			return;
+		}
+
+		this.engine = await FiltersEngine.fromLists(
+			fetch,
+			lists,
+			{
+				enableCompression: true,
+			},
+			{
+				path: "engine.bin",
+				read: fs.readFile,
+				write: fs.writeFile,
+			},
+		);
+	}
+
+	setupSession(session: Session) {
+		if (!this._engine) {
+			return;
+		}
+
+		session.webRequest.onBeforeRequest(
+			({ url, resourceType, referrer }, callback) => {
+				const { match } = this.engine.match(
+					Request.fromRawDetails({
+						url,
+						type: resourceType,
+						sourceUrl: referrer,
+					}),
+				);
+
+				callback({
+					cancel: match,
+				});
+			},
+		);
+	}
+}
+
+export const blocker = new Blocker();
